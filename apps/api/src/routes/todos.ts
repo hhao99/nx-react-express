ebf52e31bb380366dfd846938cd8877ef0dbbc59
hhao99 <hhao99@gmail.com>
2024-01-31 13:21:45 -0600

    update query by id

--- apps/api/src/routes/todos.ts
+++ apps/api/src/routes/todos.ts
@@ -30,4 +30,25 @@ router.post('/',(req,res) => {
 
 } )
 
+router.get('/:id' ,async (req,res) => {
+    // request url /todos/{id}
+    // get the id params from the url parmas
+    const id = req.params.id
+
+    /* Finds a single document by its _id field. findById(id) is almost* 
+    equivalent to findOne({ _id: id }). 
+    If you want to query by a document's _id, use findById() 
+    instead of findOne().
+    */
+    //const todo = await Todo.findOne({_id: id }).catch(console.log)
+    const todo = await Todo.findById(id).catch(console.log) 
+        
+    res.status(201).send({
+        code: 'ok',
+        status: 201,
+        message: `find the todo: ${id}`,
+        data: todo
+    })
+})
+
 export default router;
